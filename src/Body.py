70e3bf6095f1fd6a74745fdac7de347e8705f4f0
svante2001 <svante2001@gmail.com>
2024-11-11 13:56:37 +0100

    Simulation

--- src/Body.py
+++ src/Body.py
@@ -1,14 +1,17 @@
+from vpython import *
+import numpy as np
+
 class Body:
-    def __init__(self, mass, x, y, z):
+    def __init__(self, mass, location, velocity, vcolor):
         self.mass = mass
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def update_mass(self, m):
-        self.mass = m
-
-    def update_location(self, x, y, z):
-        self.x += x
-        self.y += y
-        self.z += z
+        self.location = np.array(location)
+        self.velocity = np.array(velocity)
+        
+        # Create a VPython sphere for visualization
+        self.vp_sphere = sphere(pos=vec(*self.location), 
+                                color=vcolor, 
+                                radius=self.mass)
+        
+    def update_location(self, new_location):
+        self.location = np.array(new_location)
+        self.vp_sphere.pos = vec(*self.location)
--- src/main.py
+++ src/main.py
@@ -1,25 +1,48 @@
 from Body import Body
 from vpython import *
+import numpy as np
+from scipy.integrate import solve_ivp
 
-# Initialize bodies.
-a = Body(1, 1, 0, 0)
-b = Body(1, 0, 1, 0)
-c = Body(1, 0, 0, 1)
+tf = 1000.0
+body1 = Body(1.0, [10.0, 0.0, 0.0], [-0.1, 0.0, 0.1], color.red)
+body2 = Body(1.0, [0.0, 10.0, 0.0], [0.0, 0.1, -0.1], color.green)
+body3 = Body(1.0, [0.0, 0.0, 10.0], [0.2, -0.1, 0.0], color.blue)
+y0 = np.concatenate((body1.location, body2.location, body3.location,
+                     body1.velocity, body2.velocity, body3.velocity))
 
-# Visualizing.
-bodyA = sphere(pos = vec(a.x, a.y, a.z), color = color.blue, radius = a.mass)
+canvas(width=600, height=600, background=color.white)
 
-# Creating the slider to change the masses.
-aslider = slider(bind=lambda s: a.update_mass(s.value), min=0.5, max=5, step=0.1, value=a.mass)
-labelA = wtext(text=f"Mass: {a.mass:.1f}")
+# Force function for ODE.
+def f(t, y):
+    # Positions
+    p1, p2, p3 = y[0:3], y[3:6], y[6:9]
+    
+    # Distances
+    d1 = np.linalg.norm(p1 - p2)
+    d2 = np.linalg.norm(p2 - p3)
+    d3 = np.linalg.norm(p3 - p1)
+    
+    # Calculate derivatives.
+    dydt = np.concatenate((
+        # Velocities
+        y[9:18], 
+        
+        # Accelerations.
+        body2.mass * (p2 - p1) / d1**3 + body3.mass * (p3 - p1) / d3**3,
+        body1.mass * (p1 - p2) / d1**3 + body3.mass * (p3 - p2) / d2**3,
+        body1.mass * (p1 - p3) / d3**3 + body2.mass * (p2 - p3) / d2**3
+    ))
+    return dydt
 
-# Running the simulation.
-while True:
+# Solve the ODEs.
+solution = solve_ivp(
+    f, [0.0, tf], y0, method='RK45', t_eval=np.arange(0.0, tf, 0.025) # t0, tf, and step.
+)
+
+# Visualization update loop.
+for i in range(len(solution.t)):
     rate(500)
-
-    # Updating masses.
-    bodyA.radius = a.mass
-    labelA.text = f"Mass: {a.mass:.1f}"
-
-    # Updating positions.
-    bodyA.pos = vec(a.x, a.y, a.z)
+    y = solution.y[:, i]
+    body1.update_location(y[0:3])
+    body2.update_location(y[3:6])
+    body3.update_location(y[6:9])
